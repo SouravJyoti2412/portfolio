67dd85520496e614a2384bb85b1192ff24c07a08
Sourav Jyoti Naskar <souravjoyti@gmail.com>
2023-04-19 16:36:29 +0530

    intial commit

--- style.js
+++ style.js
@@ -30,3 +30,67 @@ window.addEventListener('scroll', function() {
   AOS.init({
     duration: 1200,
   })
+
+
+let titel = document.getElementById("titel")
+let home =  document.getElementById("home_btn")
+let project_btn =  document.getElementById("project_btn")
+let eduaction_btn =  document.getElementById("eduaction_btn")
+let about_btn =  document.getElementById("about_btn")
+let contact_btn =  document.getElementById("contact_btn")
+
+let titel1 = document.getElementById("titel1")
+let home1 =  document.getElementById("home_btn1")
+let project_btn1 =  document.getElementById("project_btn1")
+let eduaction_btn1 =  document.getElementById("eduaction_btn1")
+let about_btn1 =  document.getElementById("about_btn1")
+let contact_btn1 =  document.getElementById("contact_btn1")
+
+home.addEventListener("click", function(){
+    titel.textContent =("Portfolio | home");
+})
+project_btn.addEventListener("click", function(){
+    titel.textContent =("Portfolio | Project");
+})
+eduaction_btn.addEventListener("click", function(){
+    titel.textContent =("Portfolio | Education");
+})
+about_btn.addEventListener("click", function(){
+    titel.textContent =("Portfolio | About");
+})
+contact_btn.addEventListener("click", function(){
+    titel.textContent =("Portfolio | Contact");
+    contact_btn.classList.add("active")
+})
+
+home1.addEventListener("click", function(){
+    titel.textContent =("Portfolio | home");
+})
+project_btn1.addEventListener("click", function(){
+    titel.textContent =("Portfolio | Project");
+})
+eduaction_btn1.addEventListener("click", function(){
+    titel.textContent =("Portfolio | Education");
+})
+about_btn1.addEventListener("click", function(){
+    titel.textContent =("Portfolio | About");
+})
+contact_btn1.addEventListener("click", function(){
+    titel.textContent =("Portfolio | Contact");
+    contact_btn.classList.add("active")
+})
+
+
+
+// if (element.id === "home_btn") {
+//     titel.textContent =("home");
+//   } else if (element.id === "project_btn") {
+//     titel.textContent =("project");
+//   } else if (element.id === "eduaction_btn") {
+//     titel.textContent =("education");
+//   } else if (element.id === "about_btn") {
+//     titel.textContent =("about");
+//   } else if (element.id === "contact_btn") {
+//     titel.textContent =("contact");
+//   }
+
